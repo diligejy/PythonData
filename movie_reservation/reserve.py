ecf10a810e250645a80e6b717d9e632ccea83e4a
Jinyoung <40735154+diligejy@users.noreply.github.com>
2019-08-12 22:08:04 +0900

    Add files via upload

--- movie_reservation/reserve.py
+++ movie_reservation/reserve.py
@@ -3,4 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&amp;theatercode=0056&amp;date=20190812'
 html = requests.get(url)
-print(html.text)
+soup = BeautifulSoup(html.text, 'html.parser')
+title_list = soup.select('div.info-movie')
+for i in title_list:
+    print(i.select_one('a > strong').text.strip())
