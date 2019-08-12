7b584c7f3f602dc7c8c6bddf0d5e1ce121cb09f3
Jinyoung <40735154+diligejy@users.noreply.github.com>
2019-08-12 22:20:06 +0900

    Add files via upload

---
+++ movie_reservation/movie_crawler.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&amp;theatercode=0056&amp;date=20190812'
+html = requests.get(url)
+soup = BeautifulSoup(html.text, 'html.parser')
+imax = soup.select_one('span.imax')
+if(imax):
+    print('IMAX 예매가 열렸습니다')
+else:
+    print('IMAX 예매가 아직 열리지 않았습니다.')
