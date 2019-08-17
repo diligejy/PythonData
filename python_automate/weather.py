ca3d088230bf397b3941d9232ec6917f70db4ec5
Jinyoung <40735154+diligejy@users.noreply.github.com>
2019-08-17 12:52:33 +0900

    Add files via upload

---
+++ python_automate/weather.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup as bs
+from pprint import pprint
+import requests
+
+html = requests.get('https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%82%A0%EC%94%A8')
+#pprint(html.text)
+
+soup = bs(html.text, 'html.parser')
+# pprint(soup)
+
+data1 = soup.find('div', {'class' : 'detail_box'}) # 영역 추출
+#pprint(data1)
+
+data2 = data1.findAll('dd')
+#pprint(data2[0])
+
+find_dust = data2[0].find('span', {'class' : 'num'}).text
+print(find_dust)
